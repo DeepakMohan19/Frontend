a628ca997c03cee3784ab8768af3d02890b1d94f
DeepakMohan19 <havocdeepakdeepak@gmail.com>
2026-04-16 01:43:48 +0530

    2

--- app/page.tsx
+++ app/page.tsx
@@ -3,5 +3,6 @@
 export default function Home() {
   return (
    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus ex, delectus eligendi debitis non incidunt dolorum quisquam voluptas aspernatur dolorem adipisci quae, quis corrupti! Nostrum, reprehenderit! Voluptate excepturi iste tempora.</p>
+   <button>sy</button>
   );
 }
