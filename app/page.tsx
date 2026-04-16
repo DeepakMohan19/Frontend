cd4bf6e087f05899892232421699c6be003dbd5d
DeepakMohan19 <havocdeepakdeepak@gmail.com>
2026-04-16 10:16:26 +0530

    w

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,10 @@
 export default function Home() {
   return (
   <>
-  <P>A----B</P>
-  <p>C----d----T____U</p>
-  <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Impedit a ad modi omnis earum recusandae voluptates natus at est aspernatur sapiente vero quisquam ducimus laboriosam quibusdam deserunt amet, voluptatibus nostrum?</h1>
+  <h1>
+    Login The Page
+  </h1>
   </>
+
   );
 }
