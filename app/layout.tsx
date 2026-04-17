3be38a7a4246155ca5cca268cb89fd0eae163d60
DeepakMohan19 <havocdeepakdeepak@gmail.com>
2026-04-17 10:13:48 +0530

    y

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
-        <p>jjjjjjjjjjjjj</p>
+       <header>home</header>
       </body>
       {/* Header */}
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,9 @@ export default function Home() {
   return (
     <>
       <h1>
-        Login The Page
+        Welcome to the deeepak world in  mycountry
       </h1>
+      <input type="text" name="" id="" />
       <button>Login</button>
     </>
 
